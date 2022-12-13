7411d4cde6918428b69a3d43ec351ae31eda1565
d3adl1n3 <ikateshov@gmail.com>
2022-12-13 10:39:54 +0700

    tuple parser <int, string>

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <istream>
 #include <fstream>
 #include <tuple>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
@@ -12,47 +14,61 @@ ostream &operator<<(ostream &os, tuple<typeA, typeB> const& t) {
     return os << "(" << get<0>(t) << ", " << get<1>(t) << ")" << endl;
 }
 
+// class... Types
 template<typename typeA, typename typeB>
 class CSVParser {
 public:
     struct line_iterator {
-      using iterator_category = input_iterator_tag;
-      using value_type = string;
-      using difference_type = ptrdiff_t;
-      using reference = const value_type&;
-      using pointer = const value_type*;
+        using iterator_category = input_iterator_tag;
+        using value_type = tuple<typeA, typeB>;
+        using difference_type = ptrdiff_t;
+        using reference = const value_type&;
+        using pointer = const value_type*;
 
-      line_iterator(): file(nullptr) {}
-      line_iterator(istream& in): file(&in) { ++*this; }
+        line_iterator(): file(nullptr) {}
+        line_iterator(istream& in): file(&in) { ++*this; }
 
-      reference operator*() const { return s; }
-      pointer operator->() const { return &**this; }
+        reference operator*() const { return output; }
+        pointer operator->() const { return &**this; }
 
-      line_iterator& operator++() {
-          if (!getline(*file, s)) {
-              file = nullptr;
-              //throw error
-          }
-        return *this;
-      }
+        line_iterator& operator++() {
+            if (!getline(*file, s)) {
+                file = nullptr;
+                //throw error
+            }
+            stringstream line(s);
+            string element;
+            int k = 0;
+            while (getline(line, element, ';')) {
+                if (k == 0) {
+                    get<0>(output) = stoi(element);
+                } else if (k == 1) {
+                    get<1>(output) = (typeB) element;
+                }
+                k++;
+            }
+            return *this;
+        }
 
-      line_iterator operator++(int) {
-        auto copy(*this);
-        ++*this;
-        return copy;
-      }
+        line_iterator operator++(int) {
+            auto copy(*this);
+            ++*this;
+            return copy;
+        }
 
-      friend bool operator==(const line_iterator& x, const line_iterator& y) {
-        return x.file == y.file;
-      }
+        friend bool operator==(const line_iterator& x, const line_iterator& y) {
+            return x.file == y.file;
+        }
 
-      friend bool operator!=(const line_iterator& x, const line_iterator& y) {
-        return !(x == y);
-      }
-
-     private:
-      istream* file;
-      string s;
+        friend bool operator!=(const line_iterator& x, const line_iterator& y) {
+            return !(x == y);
+        }
+        
+    private:
+        istream* file;
+        string s;
+        vector<string> arr;
+        tuple<typeA, typeB> output;
     };
     
   CSVParser(std::istream& input_file): file(input_file) {}
@@ -67,10 +83,10 @@ public:
 
 int main(int argc, const char * argv[]) {
     std::ifstream file("/Users/iliakateshov/Desktop/test_csv/test_csv/test_table.csv");
-    
     CSVParser<int, string> parser(file);
-    for (string rs : parser) {
+    for (tuple<int, string> rs : parser) {
         cout << rs << endl;
     }
     return 0;
 }
+

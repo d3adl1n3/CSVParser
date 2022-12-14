f9cb29afd268078b09fe40b9797a2fad3ef81703
d3adl1n3 <ikateshov@gmail.com>
2022-12-14 12:57:44 +0700

    code refactored

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include <string>
-#include <iterator>
 #include <istream>
 #include <fstream>
 #include <tuple>
 #include <vector>
 #include <sstream>
-#include <type_traits>
 
 using namespace std;
 
@@ -19,39 +17,36 @@ T format(string element) {
 
 template <>
 int format(string element) {
-    try {
-        return stoi(element);
-    }
-    catch (...) {
-        throw invalid_argument("type unsupported");
-        return 0;
-    }
+    try { return stoi(element); }
+    catch (...) { throw invalid_argument("type unsupported"); return 0; }
 }
 
 template <>
 float format(string element) {
-    return stof(element);
+    try { return stof(element); }
+    catch (...) { throw invalid_argument("type unsupported"); return 0;}
 }
 
 template <>
 double format(string element) {
-    return stod(element);
+    try { return stod(element); }
+    catch (...) { throw invalid_argument("type unsupported"); return 0;}
 }
 
 template <>
 long double format(string element) {
-    return stold(element);
+    try { return stold(element); }
+    catch (...) { throw invalid_argument("type unsupported"); return 0;}
 }
 
 template <>
 bool format(string element) {
-    return (element != "0");
+    try { return (element != "0"); }
+    catch (...) { throw invalid_argument("type unsupported"); return 0;}
 }
 
 template <>
-string format(string element) {
-    return element;
-}
+string format(string element) { return element; }
 
 /* iterate through tuple, set elements from line stream */
 template <size_t I = 0, typename... Ts>
@@ -97,8 +92,6 @@ std::ostream& operator <<(ostream& os, const TupleT& tp) {
 template<class... Types>
 class CSVParser {
 public:
-    struct args { using arr = vector<Types...>; };
-    
     struct line_iterator {
         using iterator_category = input_iterator_tag;
         using value_type = tuple<Types...>;
@@ -113,40 +106,24 @@ public:
         pointer operator->() const { return &**this; }
 
         line_iterator& operator++() {
-            if (!getline(*file, s)) {
-                file = nullptr;
-            }
-            line_num += 1;
-            
-            if (!s.empty())
-                s.erase(s.length()-1);
+            if (!getline(*file, s)) { file = nullptr; }
+            if (!s.empty()) {s.erase(s.length()-1);}
+            line_num++;
         
             stringstream line(s);
             try { parseString(output, line); }
-            catch (const string& e) {
-                cout << e << " row: " << line_num << endl;
-            }
+            catch (const string& e) { cout << e << " row: " << line_num << endl; }
             return *this;
         }
 
-        line_iterator operator++(int) {
-            auto copy(*this);
-            ++*this;
-            return copy;
-        }
+        line_iterator operator++(int) { auto copy(*this); ++*this; return copy; }
 
-        friend bool operator==(const line_iterator& x, const line_iterator& y) {
-            return x.file == y.file;
-        }
-
-        friend bool operator!=(const line_iterator& x, const line_iterator& y) {
-            return !(x == y);
-        }
+        friend bool operator==(const line_iterator& x, const line_iterator& y) { return x.file == y.file; }
+        friend bool operator!=(const line_iterator& x, const line_iterator& y) { return !(x == y); }
         
     private:
         istream* file;
         string s;
-        vector<string> arr;
         tuple<Types...> output;
         int line_num = 0;
     };
@@ -155,7 +132,6 @@ public:
 
   line_iterator begin() const { return line_iterator(file); }
   line_iterator end() const { return line_iterator(); }
-    
 
  private:
   std::istream& file;
@@ -165,8 +141,8 @@ public:
 int main(int argc, const char * argv[]) {
     cout << "---- First test file <int, string> ----" << endl;
     ifstream file("/Users/iliakateshov/Desktop/test_csv/test_csv/test_table1.csv");
-    CSVParser<int, int> parser(file);
-    for (tuple<int, int> rs : parser) {
+    CSVParser<int, string> parser(file);
+    for (tuple<int, string> rs : parser) {
         cout << rs << endl;
     }
     cout << endl;
